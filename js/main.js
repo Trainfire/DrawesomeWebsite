590a523c81269af3b19e212e5d84153f30415467
Aaron <aarongarcha@gmail.com>
2016-03-02 20:36:47 +0000

    Update layout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-$(document).ready(function()
-{
+$(document).ready(resizePage);
+$(window).resize(resizePage);
 
-});
+function resizePage(animate)
+{
+	var targetWidth = 1280;
+	var windowWidth = $(window).width();
+	
+	if (windowWidth > 1280)
+	{
+		targetWidth =  1280;
+	}
+	else
+	{
+		targetWidth = 960;
+	}
+	
+	$('body').animate({width:targetWidth}, 500);
+	$('canvas').animate({width:targetWidth}, 500);
+}
